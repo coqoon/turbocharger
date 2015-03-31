d66085af732f35c5447151576b8be6bf68202b57
Alec Faithfull <alef@itu.dk>
2015-03-31 17:46:31 +0200

    Replaced the model-specific case classes with methods
    cwhile, as an example, is no longer a case class, but a method returning
    a ConstructorInvocation3 hidden behind a dummy trait. This paves the way
    for turning all the real implementors of CoqTerm into case classes

--- plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/java/DecoratedJavaDocument.scala
+++ plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/java/DecoratedJavaDocument.scala
@@ -148,7 +148,7 @@ object DecoratedJavaCoqDocument {
   implicit def ptr_jToCt(a : ptr_j) = tupleToCt((a._1, a._2))
 
   sealed trait ConstructorInvocation extends CoqTerm
-  class ConstructorInvocation0(constructor : String
+  class ConstructorInvocation0(val constructor : String
       ) extends ConstructorInvocation {
     override def toString =
       Seq(constructor).mkString(" ")
@@ -174,76 +174,87 @@ object DecoratedJavaCoqDocument {
       Seq(constructor, a, b, c, d).mkString("(", " ", ")")
   }
 
-  sealed trait val_j extends CoqTerm with ConstructorInvocation
-  case class vint(a : Int) extends ConstructorInvocation1(
-      "vint", a) with val_j
-  case class vbool(a : Boolean) extends ConstructorInvocation1(
-      "vbool", a) with val_j
-  case class vptr(a : ptr_j) extends ConstructorInvocation1(
-      "vptr", a) with val_j
-  case class varr(a : arrptr_j) extends ConstructorInvocation1(
-      "varr", a) with val_j
-  case object nothing extends ConstructorInvocation0(
-      "nothing") with val_j
+  sealed trait val_j extends CoqTerm
+  object val_j {
+    def vint(a : Int) : val_j =
+      new ConstructorInvocation1("vint", a) with val_j
+    def vbool(a : Boolean) : val_j =
+      new ConstructorInvocation1("vbool", a) with val_j
+    def vptr(a : ptr_j) : val_j =
+      new ConstructorInvocation1("vptr", a) with val_j
+    def varr(a : arrptr_j) : val_j =
+      new ConstructorInvocation1("varr", a) with val_j
+    def nothing() : val_j = new ConstructorInvocation0("nothing") with val_j
+  }
 
-  sealed trait dexpr_j extends CoqTerm with ConstructorInvocation
-  case class E_val(a : val_j) extends ConstructorInvocation1(
-      "E_val", a) with dexpr_j
-  case class E_var(a : var_j) extends ConstructorInvocation1(
-      "E_var", a) with dexpr_j
-  case class E_plus(a : dexpr_j, b : dexpr_j) extends ConstructorInvocation2(
-      "E_plus", a, b) with dexpr_j
-  case class E_minus(a : dexpr_j, b : dexpr_j) extends ConstructorInvocation2(
-      "E_minus", a, b) with dexpr_j
-  case class E_times(a : dexpr_j, b : dexpr_j) extends ConstructorInvocation2(
-      "E_times", a, b) with dexpr_j
-  case class E_and(a : dexpr_j, b : dexpr_j) extends ConstructorInvocation2(
-      "E_and", a, b) with dexpr_j
-  case class E_or(a : dexpr_j, b : dexpr_j) extends ConstructorInvocation2(
-      "E_or", a, b) with dexpr_j
-  case class E_not(a : dexpr_j) extends ConstructorInvocation1(
-      "E_not", a) with dexpr_j
-  case class E_lt(a : dexpr_j, b : dexpr_j) extends ConstructorInvocation2(
-      "E_lt", a, b) with dexpr_j
-  case class E_eq(a : dexpr_j, b : dexpr_j) extends ConstructorInvocation2(
-      "E_eq", a, b) with dexpr_j
+  sealed trait dexpr_j extends CoqTerm
+  object dexpr_j {
+    def E_val(a : val_j) : dexpr_j =
+      new ConstructorInvocation1("E_val", a) with dexpr_j
+    def E_var(a : var_j) : dexpr_j =
+      new ConstructorInvocation1("E_var", a) with dexpr_j
+    def E_plus(a : dexpr_j, b : dexpr_j) : dexpr_j =
+      new ConstructorInvocation2("E_plus", a, b) with dexpr_j
+    def E_minus(a : dexpr_j, b : dexpr_j) : dexpr_j =
+      new ConstructorInvocation2("E_minus", a, b) with dexpr_j
+    def E_times(a : dexpr_j, b : dexpr_j) : dexpr_j =
+      new ConstructorInvocation2("E_times", a, b) with dexpr_j
+    def E_and(a : dexpr_j, b : dexpr_j) : dexpr_j =
+      new ConstructorInvocation2("E_and", a, b) with dexpr_j
+    def E_or(a : dexpr_j, b : dexpr_j) : dexpr_j =
+      new ConstructorInvocation2("E_or", a, b) with dexpr_j
+    def E_not(a : dexpr_j) : dexpr_j =
+      new ConstructorInvocation1("E_not", a) with dexpr_j
+    def E_lt(a : dexpr_j, b : dexpr_j) : dexpr_j =
+      new ConstructorInvocation2("E_lt", a, b) with dexpr_j
+    def E_eq(a : dexpr_j, b : dexpr_j) : dexpr_j =
+      new ConstructorInvocation2("E_eq", a, b) with dexpr_j
+  }
 
   sealed trait cmd_j extends CoqTerm with ConstructorInvocation
-  case class cassign(a : var_j, b : dexpr_j) extends ConstructorInvocation2(
-      "cassign", a, b) with cmd_j
-  case object cskip extends ConstructorInvocation0(
-      "cskip") with cmd_j
-  case class cseq(a : cmd_j, b : cmd_j) extends ConstructorInvocation2(
-      "cseq", a, b) with cmd_j
-  case class cif(a : dexpr_j, b : cmd_j, c : cmd_j) extends ConstructorInvocation3(
-      "cif", a, b, c) with cmd_j
-  case class cwhile(a : dexpr_j, b : cmd_j) extends ConstructorInvocation2(
-      "cwhile", a, b) with cmd_j
-  case class cread(a : var_j, b : var_j, c : field_j) extends ConstructorInvocation3(
-      "cread", a, b, c) with cmd_j
-  case class cwrite(a : var_j, b : field_j, c : dexpr_j) extends ConstructorInvocation3(
-      "cwrite", a, b, c) with cmd_j
-  case class carrread(a : var_j, b : var_j, c : List[dexpr_j]) extends ConstructorInvocation3(
-      "carrread", a, b, c) with cmd_j
-  case class carrwrite(a : var_j, b : List[dexpr_j], c : dexpr_j) extends ConstructorInvocation3(
-      "carrwrite", a, b, c) with cmd_j
-  case class carralloc(a : var_j, b : dexpr_j) extends ConstructorInvocation2(
-      "carralloc", a, b) with cmd_j
-  case class calloc(a : var_j, b : class_j) extends ConstructorInvocation2(
-      "calloc", a, b) with cmd_j
-  case class cdcall(a : var_j, b : var_j, c : method_j, d : List[dexpr_j]) extends ConstructorInvocation4(
-      "cdcall", a, b, c, d) with cmd_j
-  case class cscall(a : var_j, b : class_j, c : method_j, d : List[dexpr_j]) extends ConstructorInvocation4(
-      "cscall", a, b, c, d) with cmd_j
-  case class cassert(a : dexpr_j) extends ConstructorInvocation1(
-      "cassert", a) with cmd_j
+  object cmd_j {
+    def cassign(a : var_j, b : dexpr_j) : cmd_j =
+        new ConstructorInvocation2("cassign", a, b) with cmd_j
+    def cskip() : cmd_j = new ConstructorInvocation0("cskip") with cmd_j
+    def cseq(a : cmd_j, b : cmd_j) : cmd_j =
+      new ConstructorInvocation2("cseq", a, b) with cmd_j
+    def cif(a : dexpr_j, b : cmd_j, c : cmd_j) : cmd_j =
+      new ConstructorInvocation3("cif", a, b, c) with cmd_j
+    def cwhile(a : dexpr_j, b : cmd_j) : cmd_j =
+      new ConstructorInvocation2("cwhile", a, b) with cmd_j
+    def cread(a : var_j, b : var_j, c : field_j) : cmd_j =
+      new ConstructorInvocation3("cread", a, b, c) with cmd_j
+    def cwrite(a : var_j, b : field_j, c : dexpr_j) : cmd_j =
+      new ConstructorInvocation3("cwrite", a, b, c) with cmd_j
+    def carrread(a : var_j, b : var_j, c : List[dexpr_j]) : cmd_j =
+      new ConstructorInvocation3("carrread", a, b, c) with cmd_j
+    def carrwrite(a : var_j, b : List[dexpr_j], c : dexpr_j) : cmd_j =
+      new ConstructorInvocation3("carrwrite", a, b, c) with cmd_j
+    def carralloc(a : var_j, b : dexpr_j) : cmd_j =
+      new ConstructorInvocation2("carralloc", a, b) with cmd_j
+    def calloc(a : var_j, b : class_j) : cmd_j =
+      new ConstructorInvocation2("calloc", a, b) with cmd_j
+    def cdcall(a : var_j, b : var_j, c : method_j, d : List[dexpr_j]) : cmd_j =
+      new ConstructorInvocation4("cdcall", a, b, c, d) with cmd_j
+    def cscall(a : var_j,
+        b : class_j, c : method_j, d : List[dexpr_j]) : cmd_j =
+      new ConstructorInvocation4("cscall", a, b, c, d) with cmd_j
+    def cassert(a : dexpr_j) : cmd_j =
+      new ConstructorInvocation1("cassert", a) with cmd_j
+  }
+
+  import cmd_j._
+  import val_j._
+  import dexpr_j._
 
   def cseqise(cs : List[cmd_j]) : cmd_j = cs match {
     case Nil =>
       cskip
-    case `cskip` :: a =>
+    case (c : ConstructorInvocation0) :: a
+        if c.constructor == "cskip" =>
       cseqise(a)
-    case a :: `cskip` :: Nil =>
+    case a :: (c : ConstructorInvocation0) :: Nil
+        if c.constructor == "cskip" =>
       a
     case a :: Nil =>
       a
