c4e2ec52eda414c54bede589c14bcc6268295f3a
Alec Faithfull <alef@itu.dk>
2015-08-28 11:42:31 +0200

    Actually attempt to extract method pre- and postconditions
    There are still lots of problems with this code, but it's a start

--- plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/coq/ProofExtraction.scala
+++ plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/coq/ProofExtraction.scala
@@ -5,6 +5,8 @@ import dk.itu.turbocharger.parsing.DecoratedDocument
 import DecoratedDocument.Region
 
 object ProofExtraction {
+  private final val Precondition = """^<%\s*precondition:""".r.unanchored
+  private final val Postcondition = """^<%\s*postcondition:""".r.unanchored
   /* @sr is the region in which to look for specifications, @pr the region for
    * proofs, @doc the document containing those regions, @langView the
    * language-specific view of that document, and @coqView the Coq-specific
@@ -15,15 +17,31 @@ object ProofExtraction {
       doc : DecoratedDocument,
       langView : DecoratedDocument#TypedView,
       coqView : DecoratedDocument#TypedView) = {
-    var pre : Option[ArbitraryTerm] = None
-    var post : Option[ArbitraryTerm] = None
-    doc.getPartialTokens(sr) match {
-      case Some((start, tokens)) =>
-        DecoratedDocument.withPositions(
-            start, tokens).filter(t => coqView.contains(t._2))
-      case _ =>
-        Seq()
-    }
+    val (pre, post) =
+      doc.getPartialTokens(sr) match {
+        case Some((start, tokens)) =>
+          import dk.itu.turbocharger.java.Partitioning
+
+          val tp = DecoratedDocument.withPositions(
+              start, tokens).filter(t => coqView.contains(t._2))
+          /* XXX: This extraction discards position information, probably
+           * interacts badly with comments, and is generally in need of being
+           * replaced with something cleverer */
+          val pre = tp.collectFirst {
+            case (o, (pt, a @ Precondition()))
+                if pt.label.startsWith(Partitioning.Coq.ContentTypes.COQ) =>
+              a.substring(2, a.length - 2).trim.stripPrefix("precondition:")
+          }
+          val post = tp.collectFirst {
+            case (o, (pt, a @ Postcondition()))
+                if pt.label.startsWith(Partitioning.Coq.ContentTypes.COQ) =>
+              a.substring(2, a.length - 2).trim.stripPrefix("postcondition:")
+          }
+          (pre.map(ArbitraryTerm), post.map(ArbitraryTerm))
+        case _ =>
+          (None, None)
+      }
+    println(pre, post)
 
     if (!pre.isEmpty || !post.isEmpty) {
       /* This method has a precondition, postcondition, or both. Extract
