6382124e2571b4061a0ddceef80a8a135ff1fda2
Alec Faithfull <alef@itu.dk>
2015-09-22 13:59:12 +0200

    Collect PIDE command change information in the same way as Coqoon does

--- plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/java/ui/DecoratedJavaEditor.scala
+++ plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/java/ui/DecoratedJavaEditor.scala
@@ -13,16 +13,50 @@ class DecoratedJavaEditor extends TextEditor {
   private[ui] val session = new dk.itu.coqoon.ui.pide.SessionManager
   session.start
 
+  import dk.itu.coqoon.ui.utilities.UIUtils.asyncExec
   import dk.itu.coqoon.core.utilities.TryCast
   import org.eclipse.ui.IFileEditorInput
   import org.eclipse.core.resources.IFile
-  import isabelle.Document
+  import isabelle.{Command, Session, Document}
   /* XXX: copied-and-pasted from PIDECoqEditor... */
   protected[ui] def getFile() : Option[IFile] =
     TryCast[IFileEditorInput](getEditorInput).map(_.getFile)
   /* ... apart from the trailing ".v", required by Coq */
   protected[ui] def getNodeName() =
     getFile.map(file => Document.Node.Name(file.getName + ".v"))
+
+  private object CommandsLock
+  private var lastSnapshot : Option[Document.Snapshot] = None
+  private[ui] var commands : Seq[(Int, Command)] = Seq()
+
+  private def commandsUpdated(changed : Seq[Command]) =
+    asyncExec {
+      import dk.itu.coqoon.ui.pide.Responses
+      val changedResultsAndMarkup =
+        (CommandsLock synchronized {
+          val ls = lastSnapshot.get
+          for (c <- changed)
+            yield (ls.node.command_start(c), c,
+                      Responses.extractResults(ls, c),
+                      Responses.extractMarkup(ls, c))
+        })
+      println(changedResultsAndMarkup)
+    }
+
+  session.addInitialiser(session =>
+    session.commands_changed += Session.Consumer[Any]("Coqoon") {
+      case changed : Session.Commands_Changed =>
+        CommandsLock synchronized {
+          lastSnapshot = getNodeName.map(n => session.snapshot(n))
+          lastSnapshot.foreach(snapshot =>
+            commands =
+              (for (command <- snapshot.node.commands;
+                    offset <- snapshot.node.command_start(command))
+                yield (offset, command)).toSeq)
+        }
+        commandsUpdated(changed.commands.toSeq)
+      case _ =>
+    })
 
   override protected def dispose() = {
     session.stop
