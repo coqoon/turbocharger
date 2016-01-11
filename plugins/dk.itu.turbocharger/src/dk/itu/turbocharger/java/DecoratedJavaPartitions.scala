ecc50951fd0f6e73642684c0379d82196fff8170
Alec Faithfull <alef@itu.dk>
2016-01-11 16:34:26 +0100

    Added delimiter flags to the DecoratedJavaPartitions state map
    Thanks to the antiquotes, it's not precisely clear if the Coq state should
    be open or delimited. For now, we'll just pretend that it's open

--- plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/java/DecoratedJavaPartitions.scala
+++ plugins/dk.itu.turbocharger/src/dk/itu/turbocharger/java/DecoratedJavaPartitions.scala
@@ -12,20 +12,20 @@ object DecoratedJavaPartitions {
     final val COQ_STRING = s"${COQ}_string"
     final val COQ_COMMENT = s"${COQ}_comment"
   }
-  final lazy val TYPES = mapping.values.toSet.toArray
+  final lazy val TYPES = mapping.values.map(_._1).toSet.toArray
 
   import dk.itu.coqoon.ui.text.coq.CoqRecogniser
   private[DecoratedJavaPartitions] lazy val mapping = {
     import CoqRecogniser.States._
     import JavaRecogniser.States._
-    Map(java -> Types.JAVA,
-        javaChar -> Types.JAVA_CHAR,
-        javaString -> Types.JAVA_STRING,
-        javaMultiLineComment -> Types.JAVA_COMMENT,
-        javaSingleLineComment -> Types.JAVA_COMMENT,
-        coq -> Types.COQ,
-        coqString -> Types.COQ_STRING,
-        coqComment -> Types.COQ_COMMENT)
+    Map(java -> (Types.JAVA, false),
+        javaChar -> (Types.JAVA_CHAR, true),
+        javaString -> (Types.JAVA_STRING, true),
+        javaMultiLineComment -> (Types.JAVA_COMMENT, true),
+        javaSingleLineComment -> (Types.JAVA_COMMENT, true),
+        coq -> (Types.COQ, false) /* "false" isn't precisely correct... */,
+        coqString -> (Types.COQ_STRING, true),
+        coqComment -> (Types.COQ_COMMENT, true))
   }
 
   import org.eclipse.jface.text.{IDocument, IDocumentExtension3}
